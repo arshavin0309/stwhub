63f9c7752feecf5bed9250f611d7e359c928d67f
Владислав <ivanov.vladislav.yurevich@gmail.com>
2025-07-25 14:43:06 +0500

    add

--- app/js/src/main.js
+++ app/js/src/main.js
@@ -12,3 +12,24 @@ let mainChooseSwiper = new Swiper(".main-choose__swiper", {
         }
     }
 });
+
+const OFFSET = 130 + 85; // сдвиг вверх на 130 пикселей
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        const targetID = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetID);
+
+        if (targetElement) {
+            e.preventDefault();
+
+            const elementPosition = targetElement.getBoundingClientRect().top + window.pageYOffset;
+            const offsetPosition = elementPosition - OFFSET;
+
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
